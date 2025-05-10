04ff86cd6d8fa6e5575df819a1a1772314329427
Provismet <26700920-Provismet@users.noreply.gitlab.com>
2025-05-10 04:39:25 +0100

    Use correct namespace.

--- src/main/java/org/dystoria/tweaks/DystoriaTweaksClient.java
+++ src/main/java/org/dystoria/tweaks/DystoriaTweaksClient.java
@@ -2,12 +2,17 @@ package org.dystoria.tweaks;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DystoriaTweaksClient implements ClientModInitializer {
-	public static final String MODID = "cobblemon-addon";
+	public static final String MODID = "dystoria-tweaks";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	public static Identifier identifier (String path) {
+		return Identifier.of(MODID, path);
+	}
 
 	@Override
 	public void onInitializeClient () {
