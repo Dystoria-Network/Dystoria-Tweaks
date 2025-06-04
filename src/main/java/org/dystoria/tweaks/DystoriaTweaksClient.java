443f9935155ec108c707f6db67305cdbfc4f1b2e
Provismet <26700920-Provismet@users.noreply.gitlab.com>
2025-06-04 20:55:27 +0100

    Add battle scoreboard to HUD.

--- src/main/java/org/dystoria/tweaks/DystoriaTweaksClient.java
+++ src/main/java/org/dystoria/tweaks/DystoriaTweaksClient.java
@@ -2,9 +2,11 @@ package org.dystoria.tweaks;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
+import org.dystoria.tweaks.battle.BattleHud;
 import org.dystoria.tweaks.resources.DystoriaResourceListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,5 +22,6 @@ public class DystoriaTweaksClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient () {
 		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new DystoriaResourceListener());
+		HudRenderCallback.EVENT.register(BattleHud::hudCallback);
 	}
 }
---
+++ src/main/java/org/dystoria/tweaks/battle/BattleHud.java
@@ -0,0 +1,77 @@
+package org.dystoria.tweaks.battle;
+
+import com.cobblemon.mod.common.api.battles.model.actor.ActorType;
+import com.cobblemon.mod.common.client.CobblemonClient;
+import com.cobblemon.mod.common.client.battle.ClientBattle;
+import com.cobblemon.mod.common.client.battle.ClientBattleActor;
+import com.cobblemon.mod.common.client.battle.ClientBattleSide;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.scoreboard.ReadableScoreboardScore;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardDisplaySlot;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.scoreboard.number.StyledNumberFormat;
+import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BattleHud {
+    private static final char ALIVE = '\u234D';
+    private static final char STATUS ='\u234E';
+    private static final char FAINTED = '\u234F';
+
+    private static final Identifier ALIVE_ICON = Identifier.of("dystorian-extras", "textures/font/alive.png");
+    private static final Identifier STATUS_ICON = Identifier.of("dystorian-extras", "textures/font/status.png");;
+    private static final Identifier FAINTED_ICON = Identifier.of("dystorian-extras", "textures/font/dead.png");;
+
+    private static final int ICON_LENGTH = 16;
+
+    public static void hudCallback (DrawContext context, RenderTickCounter counter) {
+        ClientBattle battle = CobblemonClient.INSTANCE.getBattle();
+        if (battle != null) {
+            renderSide(context, battle.getSide1(), true);
+            renderSide(context, battle.getSide2(), false);
+        }
+    }
+
+    private static void renderSide (DrawContext context, ClientBattleSide side, boolean isLeft) {
+        ClientWorld world = MinecraftClient.getInstance().world;
+        if (world == null) return;
+
+        StringBuilder team = new StringBuilder();
+
+        for (ClientBattleActor actor : side.getActors()) {
+            if (actor.getType() != ActorType.PLAYER) continue;
+            PlayerEntity player = world.getPlayerByUuid(actor.getUuid());
+            if (player == null) continue;
+
+            Scoreboard scoreboard = player.getScoreboard();
+            ScoreboardObjective teamScoreboard = scoreboard.getObjectiveForSlot(ScoreboardDisplaySlot.BELOW_NAME);
+            if (teamScoreboard == null) continue;
+
+            ReadableScoreboardScore readableScoreboardScore = scoreboard.getScore(player, teamScoreboard);
+            team.append(ReadableScoreboardScore.getFormattedScore(readableScoreboardScore, teamScoreboard.getNumberFormatOr(StyledNumberFormat.EMPTY)).getString().strip());
+        }
+
+        List<Identifier> icons = new ArrayList<>();
+        for (char icon : team.toString().toCharArray()) {
+            if (icon == ALIVE) icons.add(ALIVE_ICON);
+            else if (icon == STATUS) icons.add(STATUS_ICON);
+            else if (icon == FAINTED) icons.add(FAINTED_ICON);
+        }
+
+        int height = MinecraftClient.getInstance().getWindow().getScaledHeight();
+        int x = isLeft ? 2 : MinecraftClient.getInstance().getWindow().getScaledWidth() - ICON_LENGTH - 2;
+        for (int i = 0; i < icons.size(); ++i) {
+            Identifier icon = icons.get(i);
+            int y = (height / 2) - (icons.size() * ICON_LENGTH / 2) + (i * (ICON_LENGTH + 1));
+
+            context.drawTexture(icon, x, y, 0, 0, ICON_LENGTH, ICON_LENGTH, ICON_LENGTH, ICON_LENGTH);
+        }
+    }
+}
