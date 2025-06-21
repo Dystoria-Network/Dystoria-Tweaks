926b86d5335d11d4102c837aa2e7bd5c928f4351
Provismet <26700920-Provismet@users.noreply.gitlab.com>
2025-06-21 04:46:17 +0100

    Add tera icons to PC menu.

--- src/main/java/org/dystoria/tweaks/mixin/PCGUIMixin.java
+++ src/main/java/org/dystoria/tweaks/mixin/PCGUIMixin.java
@@ -1,11 +1,15 @@
 package org.dystoria.tweaks.mixin;
 
+import com.cobblemon.mod.common.api.gui.GuiUtilsKt;
 import com.cobblemon.mod.common.client.gui.pc.PCGUI;
 import com.cobblemon.mod.common.pokemon.Pokemon;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
 import org.dystoria.tweaks.icon.ShinyIcons;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import org.dystoria.tweaks.icon.TeraIcons;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,7 +25,7 @@ public abstract class PCGUIMixin extends Screen {
     @Shadow private Pokemon previewPokemon;
 
     @Inject(method = "render", at = @At("TAIL"))
-    private void addShinierIcons (DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo info) {
+    private void addExtraIcons (DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo info) {
         if (this.previewPokemon != null) {
             int x = (super.width - 349) / 2;
             int y = (super.height - 205) / 2;
@@ -34,6 +38,30 @@ public abstract class PCGUIMixin extends Screen {
                 16,
                 false
             );
+
+            // Tera
+            double teraX = (x + 61) / 0.5;
+            double teraY = (y + 93.5) / 0.5;
+            Identifier teraType = TeraIcons.getTeraIcon(this.previewPokemon.getTeraType().showdownId());
+            GuiUtilsKt.blitk(
+                context.getMatrices(),
+                teraType,
+                teraX, teraY,
+                TeraIcons.LENGTH, TeraIcons.LENGTH,
+                0, 0,
+                TeraIcons.LENGTH, TeraIcons.LENGTH,
+                0,
+                1, 1, 1,
+                1,
+                true,
+                0.5f
+            );
+
+            if (mouseX * 2f >= teraX && mouseX * 2f <= teraX + TeraIcons.LENGTH && mouseY * 2f >= teraY && mouseY * 2f <= teraY + TeraIcons.LENGTH) {
+                char[] characters = this.previewPokemon.getTeraType().showdownId().toCharArray();
+                if (characters.length > 0) characters[0] = Character.toUpperCase(characters[0]);
+                context.drawTooltip(MinecraftClient.getInstance().textRenderer, Text.translatable("tooltip.dystoria-tweaks.tera", new String(characters)), mouseX, mouseY);
+            }
         }
     }
 }
