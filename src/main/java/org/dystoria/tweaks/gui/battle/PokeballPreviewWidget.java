25e55ac3b0773afdfdcb70ee2236f37161370438
Provismet <17149901+Provismet@users.noreply.github.com>
2026-01-12 18:08:38 +0000

    Add active pokemon to pokeball widget

--- src/main/java/org/dystoria/tweaks/gui/battle/PokeballPreviewWidget.java
+++ src/main/java/org/dystoria/tweaks/gui/battle/PokeballPreviewWidget.java
@@ -1,5 +1,6 @@
 package org.dystoria.tweaks.gui.battle;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.widget.ClickableWidget;
@@ -16,20 +17,26 @@ public class PokeballPreviewWidget extends ClickableWidget {
 
     private static final Identifier ALIVE_LEFT = DystoriaTweaksClient.identifier("textures/gui/battle/alive_left.png");
     private static final Identifier ALIVE_LEFT_HOVERED = DystoriaTweaksClient.identifier("textures/gui/battle/alive_left_hovered.png");
+    private static final Identifier ALIVE_LEFT_ACTIVE = DystoriaTweaksClient.identifier("textures/gui/battle/alive_left_active.png");
     private static final Identifier ALIVE_RIGHT = DystoriaTweaksClient.identifier("textures/gui/battle/alive_right.png");
     private static final Identifier ALIVE_RIGHT_HOVERED = DystoriaTweaksClient.identifier("textures/gui/battle/alive_right_hovered.png");
+    private static final Identifier ALIVE_RIGHT_ACTIVE = DystoriaTweaksClient.identifier("textures/gui/battle/alive_right_active.png");
     private static final Identifier ALIVE_HP = DystoriaTweaksClient.identifier("textures/gui/battle/alive_health_bar.png");
 
     private static final Identifier STATUS_LEFT = DystoriaTweaksClient.identifier("textures/gui/battle/status_left.png");
     private static final Identifier STATUS_LEFT_HOVERED = DystoriaTweaksClient.identifier("textures/gui/battle/status_left_hovered.png");
+    private static final Identifier STATUS_LEFT_ACTIVE = DystoriaTweaksClient.identifier("textures/gui/battle/status_left_active.png");
     private static final Identifier STATUS_RIGHT = DystoriaTweaksClient.identifier("textures/gui/battle/status_right.png");
     private static final Identifier STATUS_RIGHT_HOVERED = DystoriaTweaksClient.identifier("textures/gui/battle/status_right_hovered.png");
+    private static final Identifier STATUS_RIGHT_ACTIVE = DystoriaTweaksClient.identifier("textures/gui/battle/status_right_active.png");
     private static final Identifier STATUS_HP = DystoriaTweaksClient.identifier("textures/gui/battle/status_health_bar.png");
 
     private static final Identifier FAINTED_LEFT = DystoriaTweaksClient.identifier("textures/gui/battle/fainted_left.png");
     private static final Identifier FAINTED_LEFT_HOVERED = DystoriaTweaksClient.identifier("textures/gui/battle/fainted_left_hovered.png");
+    private static final Identifier FAINTED_LEFT_ACTIVE = DystoriaTweaksClient.identifier("textures/gui/battle/fainted_left_active.png");
     private static final Identifier FAINTED_RIGHT = DystoriaTweaksClient.identifier("textures/gui/battle/fainted_right.png");
     private static final Identifier FAINTED_RIGHT_HOVERED = DystoriaTweaksClient.identifier("textures/gui/battle/fainted_right_hovered.png");
+    private static final Identifier FAINTED_RIGHT_ACTIVE = DystoriaTweaksClient.identifier("textures/gui/battle/fainted_right_active.png");
 
     private final BattlePokemonMemory underlying;
     private final boolean isLeft;
@@ -42,32 +49,43 @@ public class PokeballPreviewWidget extends ClickableWidget {
 
     @Override
     protected void renderWidget (DrawContext context, int mouseX, int mouseY, float delta) {
-        if (this.underlying == null || this.underlying.getState() == null) return;
+        if (this.underlying == null) return;
 
         Identifier texture;
         if (this.isHovered()) {
-            if (!this.underlying.getState().isAlive()) texture = this.isLeft ? FAINTED_LEFT_HOVERED : FAINTED_RIGHT_HOVERED;
-            else if (this.underlying.getState().status().isPresent()) texture = this.isLeft ? STATUS_LEFT_HOVERED : STATUS_RIGHT_HOVERED;
+            if (!this.underlying.isAlive()) texture = this.isLeft ? FAINTED_LEFT_HOVERED : FAINTED_RIGHT_HOVERED;
+            else if (this.underlying.hasStatus()) texture = this.isLeft ? STATUS_LEFT_HOVERED : STATUS_RIGHT_HOVERED;
             else texture = this.isLeft ? ALIVE_LEFT_HOVERED : ALIVE_RIGHT_HOVERED;
 
-            this.underlying.render(context, this.isLeft ? this.getX() + WIDTH : this.getX() - WIDTH - BattlePokemonMemory.RENDER_WIDTH, this.getY(), delta, this.isLeft);
+            this.underlying.render(
+                context,
+                this.isLeft ? this.getX() + WIDTH + 5 : this.getX() - BattlePokemonMemory.PANEL_WIDTH - 5,
+                (MinecraftClient.getInstance().getWindow().getScaledHeight() - BattlePokemonMemory.PANEL_HEIGHT) / 2,
+                delta,
+                this.isLeft
+            );
+        }
+        else if (this.underlying.isActive()) {
+            if (!this.underlying.isAlive()) texture = this.isLeft ? FAINTED_LEFT_ACTIVE : FAINTED_RIGHT_ACTIVE;
+            else if (this.underlying.hasStatus()) texture = this.isLeft ? STATUS_LEFT_ACTIVE : STATUS_RIGHT_ACTIVE;
+            else texture = this.isLeft ? ALIVE_LEFT_ACTIVE : ALIVE_RIGHT_ACTIVE;
         }
         else {
-            if (!this.underlying.getState().isAlive()) texture = this.isLeft ? FAINTED_LEFT : FAINTED_RIGHT;
-            else if (this.underlying.getState().status().isPresent()) texture = this.isLeft ? STATUS_LEFT : STATUS_RIGHT;
+            if (!this.underlying.isAlive()) texture = this.isLeft ? FAINTED_LEFT : FAINTED_RIGHT;
+            else if (this.underlying.hasStatus()) texture = this.isLeft ? STATUS_LEFT : STATUS_RIGHT;
             else texture = this.isLeft ? ALIVE_LEFT : ALIVE_RIGHT;
         }
         context.drawTexture(texture, this.getX(), this.getY(), 0, 0, 0, this.getWidth(), this.getHeight(), this.getWidth(), this.getHeight());
 
-        if (this.underlying.getState().isAlive()) {
-            Identifier hpTexture = this.underlying.getState().status().isPresent() ? STATUS_HP : ALIVE_HP;
+        if (this.underlying.isAlive()) {
+            Identifier hpTexture = this.underlying.hasStatus() ? STATUS_HP : ALIVE_HP;
 
             int hpX = this.isLeft ? this.getX() + 1 : this.getX() + WIDTH - 2;
-            int hpHeight = (int)(this.underlying.getState().healthPercentage() * HP_HEIGHT);
+            int hpHeight = (int)(this.underlying.lerpHealthPercentage(delta) * HP_HEIGHT);
             context.drawTexture(
                 hpTexture,
                 hpX, this.getY() + 4 + (HP_HEIGHT - hpHeight), 1,
-                0, 1f - (float)this.underlying.getState().healthPercentage(),
+                0, 1f - (float)this.underlying.getHealthPercentage(),
                 HP_WIDTH, hpHeight,
                 HP_WIDTH, hpHeight
             );
@@ -82,5 +100,9 @@ public class PokeballPreviewWidget extends ClickableWidget {
     @Override
     protected boolean isValidClickButton (int button) {
         return false;
+    }
+
+    public BattlePokemonMemory getBattleMemory () {
+        return this.underlying;
     }
 }
