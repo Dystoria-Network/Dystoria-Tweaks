e7a3d7959cd128b5de11b3d8b77f5d0e81d50433
Provismet <17149901+Provismet@users.noreply.github.com>
2026-03-16 21:09:20 +0000

    Add move inspector

---
+++ src/main/java/org/dystoria/tweaks/mixin/MoveTileMixin.java
@@ -0,0 +1,30 @@
+package org.dystoria.tweaks.mixin;
+
+import com.cobblemon.mod.common.api.moves.MoveTemplate;
+import com.cobblemon.mod.common.client.gui.battle.subscreen.BattleMoveSelection;
+import net.minecraft.client.gui.DrawContext;
+import org.dystoria.tweaks.gui.battle.MovePreviewWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BattleMoveSelection.MoveTile.class)
+public abstract class MoveTileMixin {
+    @Shadow
+    public abstract boolean isHovered(double mouseX, double mouseY);
+
+    @Shadow private MoveTemplate moveTemplate;
+
+    @Unique MovePreviewWidget previewWidget;
+
+    @Inject(method = "render", at = @At("TAIL"))
+    private void addPreview (DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo info) {
+        if (this.isHovered(mouseX, mouseY)) {
+            if (this.previewWidget == null) this.previewWidget = new MovePreviewWidget(this.moveTemplate);
+            this.previewWidget.render(context, mouseX, mouseY, delta);
+        }
+    }
+}
