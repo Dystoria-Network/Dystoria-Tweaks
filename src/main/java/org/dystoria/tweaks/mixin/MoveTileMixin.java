377630090cd448492ef2bfcbd56fdda15aa197bb
Provismet <17149901+Provismet@users.noreply.github.com>
2026-03-16 21:11:44 +0000

    Make move tooltips configurable

--- src/main/java/org/dystoria/tweaks/mixin/MoveTileMixin.java
+++ src/main/java/org/dystoria/tweaks/mixin/MoveTileMixin.java
@@ -3,6 +3,7 @@ package org.dystoria.tweaks.mixin;
 import com.cobblemon.mod.common.api.moves.MoveTemplate;
 import com.cobblemon.mod.common.client.gui.battle.subscreen.BattleMoveSelection;
 import net.minecraft.client.gui.DrawContext;
+import org.dystoria.tweaks.config.DystoriaTweaksConfig;
 import org.dystoria.tweaks.gui.battle.MovePreviewWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,7 +23,7 @@ public abstract class MoveTileMixin {
 
     @Inject(method = "render", at = @At("TAIL"))
     private void addPreview (DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo info) {
-        if (this.isHovered(mouseX, mouseY)) {
+        if (this.isHovered(mouseX, mouseY) && DystoriaTweaksConfig.shouldRenderMoveTooltips()) {
             if (this.previewWidget == null) this.previewWidget = new MovePreviewWidget(this.moveTemplate);
             this.previewWidget.render(context, mouseX, mouseY, delta);
         }
